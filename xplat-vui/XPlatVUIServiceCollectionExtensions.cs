90207c7106266171b0330f122d0267f0da4f61c9
Takumasa Hirabayashi <baa_ram_ewe_shipping@live.jp>
2019-09-25 15:13:20 +0900

    0.1.2 remove new constraint from AssistantBase

--- xplat-vui/XPlatVUIServiceCollectionExtensions.cs
+++ xplat-vui/XPlatVUIServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ namespace XPlat.VUI
     {
         public static IServiceCollection AddAssistant<T1, T2>(this IServiceCollection services)
            where T1 : class, IAssistant
-           where T2 : AssistantBase, T1, new()
+           where T2 : AssistantBase, T1
         {
             return services.AddScoped<T1, T2>();
         }
